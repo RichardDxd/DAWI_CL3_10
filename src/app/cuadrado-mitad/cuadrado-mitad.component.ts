b58aafc1b9903d46bf16f966dc4025f74de7a920
RichardDxd <rlandaamao@gmail.com>
2024-05-11 13:33:40 -0500

    avance3

--- src/app/cuadrado-mitad/cuadrado-mitad.component.ts
+++ src/app/cuadrado-mitad/cuadrado-mitad.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule  } from '@angular/common';
-
+import {CommonModule} from '@angular/common'
 @Component({
   selector: 'app-cuadrado-mitad',
   standalone: true,
